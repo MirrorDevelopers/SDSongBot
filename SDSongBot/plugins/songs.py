65b7035916e258cf1856dd1ee722c4690642cf45
DarkZ <93233687+MirrorDevelopers@users.noreply.github.com>
2021-12-14 15:51:20 +0530

    Update songs.py

--- SDSongBot/plugins/songs.py
+++ SDSongBot/plugins/songs.py
@@ -15,7 +15,7 @@ def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
-@app.on_message(filters.command('dsong'))
+@app.on_message(filters.command('find'))
 def song(client, message):
 
     user_id = message.from_user.id 
@@ -26,7 +26,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('**🔎Searching...**')
+    m = message.reply('**🔎Searching The Song...**')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -48,13 +48,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("**Downloading the song by @SDbotsz**")
+    m.edit("**Downloading the song by @FinitySongBot**")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**Uploaded** by @SDbotsz.**'
+        rep = '**Uploaded** by @FinitySongBot'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -62,7 +62,7 @@ def song(client, message):
         s = message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit('❌ Error')
+        m.edit('❌ Error ')
         print(e)
 
     try:
